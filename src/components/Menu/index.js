2b7e92560f4c6db842a7f567ed10aa8b8f8fec11
Renan Oliveira <renan04lima@outlook.com>
2020-08-01 14:09:10 -0300

    update para o heroku

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Logo from '../../assets/img/logo.png'
-import Button from '../Button'
-import './Menu.css'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/logo.png';
+import Button from '../Button';
+import './Menu.css';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="Renan Flix logo"></img>
-            </Link>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="Renan Flix logo" />
+      </Link>
 
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Vídeo
-            </Button>
-        </nav>
-    )
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
-export default Menu
+export default Menu;
